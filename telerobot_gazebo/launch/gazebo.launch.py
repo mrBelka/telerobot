e901e600c5696fe8cec4f20870a23f0c535c4277
mishklgpmi <mishklgpmi@mail.ru>
2023-04-24 12:52:24 +0200

    refactoring

--- telerobot_gazebo/launch/gazebo.launch.py
+++ telerobot_gazebo/launch/gazebo.launch.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2021 Juan Miguel Jimeno
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
@@ -24,20 +10,16 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     use_sim_time = True
 
-    joy_launch_path = PathJoinSubstitution(
-        [FindPackageShare('linorobot2_bringup'), 'launch', 'joy_teleop.launch.py']
-    )
-
     ekf_config_path = PathJoinSubstitution(
         [FindPackageShare("linorobot2_base"), "config", "ekf.yaml"]
     )
 
     world_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_gazebo"), "worlds", "test.world"]
+        [FindPackageShare("telerobot_gazebo"), "worlds", "test.world"]
     )
 
     description_launch_path = PathJoinSubstitution(
-        [FindPackageShare('linorobot2_description'), 'launch', 'description.launch.py']
+        [FindPackageShare('telerobot_description'), 'launch', 'description.launch.py']
     )
 
     return LaunchDescription([
@@ -51,14 +33,14 @@ def generate_launch_description():
             executable='spawn_entity.py',
             name='urdf_spawner',
             output='screen',
-            arguments=["-topic", "robot_description", "-entity", "linorobot2"]
+            arguments=["-topic", "robot_description", "-entity", "telerobot"]
         ),
 
-        Node(
-            package='linorobot2_gazebo',
-            executable='command_timeout.py',
-            name='command_timeout'
-        ),
+        #Node(
+        #    package='telerobot_gazebo',
+        #    executable='command_timeout.py',
+        #    name='command_timeout'
+        #),
 
         Node(
             package='robot_localization',
@@ -79,13 +61,4 @@ def generate_launch_description():
                 'publish_joints': 'false',
             }.items()
         ),
-
-        #IncludeLaunchDescription(
-        #    PythonLaunchDescriptionSource(joy_launch_path),
-        #)
     ])
-
-#sources: 
-#https://navigation.ros.org/setup_guides/index.html#
-#https://answers.ros.org/question/374976/ros2-launch-gazebolaunchpy-from-my-own-launch-file/
-#https://github.com/ros2/rclcpp/issues/940
