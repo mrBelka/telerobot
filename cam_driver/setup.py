d063f3ff4746ce0eb597192f196c2ce63f5a1050
mishklgpmi <mishklgpmi@mail.ru>
2024-06-01 13:50:19 +0200

    add cam_driver

---
+++ cam_driver/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'cam_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='akoshelev2004@yandex,ru',
+    description='Camera video tanslation',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'cam_driver = cam_driver.translation_server:main'
+        ],
+    },
+)
