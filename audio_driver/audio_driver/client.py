d69fddebdeec82efb5e575a0bbc9c7f0c665678c
mishklgpmi <mishklgpmi@mail.ru>
2024-06-28 16:39:02 +0200

    add exceptions

--- audio_driver/audio_driver/client.py
+++ audio_driver/audio_driver/client.py
@@ -1,48 +1,54 @@
 import socket, pyaudio, pickle, struct
 
 def main(args=None):
-    CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
 
     # Socket create
-    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host_ip = '192.168.1.139'
     port = 1234
-    client_socket.connect((host_ip,port))
-    data = b""
-    payload_size = struct.calcsize("Q")
-    p = pyaudio.PyAudio()
-    stream_out = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True)
 
-    while True:
-        while len(data) < payload_size:
-            packet = client_socket.recv(4*1024)
-            if not packet:
-                break
-            data += packet
-            
-        if len(data) >= payload_size:
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack("Q", packed_msg_size)[0]
-            
-            while len(data) < msg_size:
-                data += client_socket.recv(4*1024)
+    try:
+        client_socket.connect((host_ip,port))
+        data = b""
+        payload_size = struct.calcsize("Q")
+        p = pyaudio.PyAudio()
+        stream_out = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        output=True)
+
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4*1024)
+                if not packet:
+                    break
+                data += packet
                 
-            frame_data = data[:msg_size]
-            data = data[msg_size:]
-            frame = pickle.loads(frame_data)
-            if frame:
-                stream_out.write(frame)
+            if len(data) >= payload_size:
+                packed_msg_size = data[:payload_size]
+                data = data[payload_size:]
+                msg_size = struct.unpack("Q", packed_msg_size)[0]
+                
+                while len(data) < msg_size:
+                    data += client_socket.recv(4*1024)
+                    
+                frame_data = data[:msg_size]
+                data = data[msg_size:]
+                frame = pickle.loads(frame_data)
+                if frame is not None:
+                    stream_out.write(frame)
 
-    stream_out.stop_stream()
-    stream_out.close()
-    client_socket.close()
+    except Exception as e:
+        print(f"Server connection error: {e}")
+
+    finally:
+        stream_out.stop_stream()
+        stream_out.close()
+        client_socket.close()
+        p.terminate()
 
 if __name__ == '__main__':
     main()
--- audio_driver/audio_driver/server.py
+++ audio_driver/audio_driver/server.py
@@ -7,41 +7,51 @@ def main(args=None):
     RATE = 44100
 
     # Socket create
-    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    host_name = socket.gethostname()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host_ip = '192.168.1.139'
     print('HOST IP:', host_ip)
     port = 1234
     socket_address = (host_ip,port)
 
-    # Socket bind
-    server_socket.bind(socket_address)
+    try:
+        # Socket bind
+        server_socket.bind(socket_address)
 
-    # Socket listen
-    server_socket.listen(5)
-    print("LISTENING AT:", socket_address)
+        # Socket listen
+        server_socket.listen(5)
+        print("LISTENING AT:", socket_address)
 
-    # Socket accept
-    while True:
-        client_socket,addr = server_socket.accept()
-        print('GOT CONNECTION FROM:',addr)
-        if client_socket:
-            p = pyaudio.PyAudio()
-            
-            stream_in = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-            
-            while True:
-                data = stream_in.read(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q", len(a))+a
-                client_socket.sendall(message)
-    stream_in.stop_stream()
-    stream_in.close()
-    client_socket.close()
+        # Socket accept
+        while True:
+            client_socket,addr = server_socket.accept()
+            print('GOT CONNECTION FROM:', addr)
+            if client_socket:
+                p = pyaudio.PyAudio()
+                
+                stream_in = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+                
+                try:
+                    while True:
+                        data = stream_in.read(CHUNK)
+                        a = pickle.dumps(data)
+                        message = struct.pack("Q", len(a))+a
+                        client_socket.sendall(message)
+                        
+                except Exception as e:
+                    print(f"Data sending error: {e}")
+
+                finally:
+                    stream_in.stop_stream()
+                    stream_in.close()
+                    client_socket.close()
+                    p.terminate()
+
+    except Exception as e:
+        print(f"Server start error: {e}")
 
 if __name__ == '__main__':
     main()
