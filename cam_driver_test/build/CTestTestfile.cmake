d85e44427e0775ee9bdb9ec7a30fd341257e9d85
mishklgpmi <mishklgpmi@mail.ru>
2024-08-05 14:47:18 +0200

    delete test dir

--- cam_driver_test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/base1/ros2_ws/src/cpp_pubsub
-# Build directory: /home/base1/ros2_ws/src/cpp_pubsub/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
